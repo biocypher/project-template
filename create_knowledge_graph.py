8ed39ba1d97be2997efff5757a172d4144631133
slobentanzer <sebastian.lobentanzer@gmail.com>
2024-02-27 16:47:11 +0100

    update template script to use resource cache

--- create_knowledge_graph.py
+++ create_knowledge_graph.py
@@ -1,4 +1,4 @@
-from biocypher import BioCypher
+from biocypher import BioCypher, Resource
 from template_package.adapters.example_adapter import (
     ExampleAdapter,
     ExampleAdapterNodeType,
@@ -11,6 +11,17 @@ from template_package.adapters.example_adapter import (
 # You can use `config/biocypher_config.yaml` to configure the framework or
 # supply settings via parameters below
 bc = BioCypher()
+
+# Download and cache resources (change the directory in the options if needed)
+urls = "https://file-examples.com/wp-content/storage/2017/02/file_example_CSV_5000.csv"
+resource = Resource(
+    name="Example resource",  # Name of the resource
+    url_s=urls,  # URL to the resource(s)
+    lifetime=7,  # seven days cache lifetime
+)
+paths = bc.download(resource)  # Downloads to '.cache' by default
+print(paths)
+# You can use the list of paths returned to read the resource into your adapter
 
 # Choose node types to include in the knowledge graph.
 # These are defined in the adapter (`adapter.py`).
